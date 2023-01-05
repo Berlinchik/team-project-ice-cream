37d328b8e92fcfd79f6850b1c2625c9a369cc09c
Berlinchik <davidarts11@gmail.com>
2023-01-05 23:43:38 +0200

    create-team-project

---
+++ dist/index.0d916b25.js
@@ -0,0 +1,15 @@
+(()=>{
+    const refs = {
+        openModalBtns: document.querySelectorAll("[data-modal-buy-open]"),
+        closeModalBtn: document.querySelector("[data-modal-buy-close]"),
+        modal: document.querySelector("[data-modal-buy]")
+    };
+    for (item of refs.openModalBtns)item.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        document.body.classList.toggle("is-scroll");
+    }
+})();
+
+//# sourceMappingURL=index.0d916b25.js.map
